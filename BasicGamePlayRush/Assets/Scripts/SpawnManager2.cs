7200c351c3d0d363f52b381a810e9887c494c61c
jezeenaaa <724214@student.dusd.net>
2023-03-08 15:30:19 -0800

    Finished basicgameplay
    finished game

--- BasicGamePlayRush/Assets/Scripts/SpawnManager2.cs
+++ BasicGamePlayRush/Assets/Scripts/SpawnManager2.cs
@@ -2,25 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
+public class SpawnManager2 : MonoBehaviour
 {
+
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 10;
     private float spawnPosZ = 20;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        InvokeRepeating("SpawnRandonAnimal", startDelay, spawnInterval);
 
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void SpawnRandonAnimal()
     {
